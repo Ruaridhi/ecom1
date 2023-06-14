12ddf89db5910a466f6abcfd5f9e9efc7ac899d7
Ruaridhi <ruaridhiscanlain@yahoo.co.uk>
2023-06-14 11:01:45 +0100

    basic home page structure

---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>All Rights Reserved. Copyright © 2022</p>
+    </footer>
+  );
+};
+export default Footer;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import Header from './Header';
 import Footer from './Footer';
+import Home from '../pages/Home';
 import { PropsWithChildren } from 'react';
 
 export default function Layout({ children }: PropsWithChildren<any>) {
   return (
     <>
       <Header />
+      <Home />
       <main>{children}</main>
       <Footer />
     </>
